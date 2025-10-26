bda4eec902ee0d7d36889c52523776ea50445f97
Debiprasadnandi <danna.debiprasad2005@gmail.com>
2025-10-26 20:49:20 +0530

    first variable

---
+++ 01_variable.js
@@ -0,0 +1,16 @@
+const accountId = 123456
+let accountEmail = "user@example.com"
+var accountPassword = "234455"
+accountCity = "bankura"
+
+console.log(accountId)
+
+/*accountId = 3445
+console.log(accountId) // this is not allowed
+*/
+
+accountEmail = "rocky@gmail.com"
+accountPassword = "987654"
+accountCity = "kolkata"
+
+console.table([accountId, accountEmail, accountPassword, accountCity]) // we use it beacause of print all the const and var
